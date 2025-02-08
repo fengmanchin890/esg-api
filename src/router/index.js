025ed0fb64e4ed691263b6e70c200979667d7706
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2025-02-08 14:57:21 +0700

    fixed login

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,45 @@
-// router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Input from '../views/Input.vue';
-import Analyze from '../views/Analyze.vue';
-import LoginPage from '../views/Login.vue';
-import useUser from '@/hooks/useUser';
-const { isAuthenticated } = useUser();
+  // router/index.js
+  import { createRouter, createWebHistory } from 'vue-router';
+  import Home from '../views/Home.vue';
+  import Input from '../views/Input.vue';
+  import Analyze from '../views/Analyze.vue';
+  import LoginPage from '../views/Login.vue';
+  import useUser from '@/hooks/useUser';
+  const { isAuthenticated } = useUser();
 
-const routes = [
-  {
-    path: '/',
-    component: LoginPage,
-    beforeEnter: (to, from, next) => {
-      if (isAuthenticated()) {
-        next('/home');
-      } else {
-        next();
-      }
+  const routes = [
+    {
+      path: '/',
+      component: LoginPage,
+      beforeEnter: (to, from, next) => {
+        if (isAuthenticated()) {
+          next('/home');
+        } else {
+          next();
+        }
+      },
     },
-  },
-  {
-    path: '/home',
-    component: Home,
-    beforeEnter: (to, from, next) => {
-      if (isAuthenticated()) {
-        next();
-      } else {
-        next('/');
-      }
+    {
+      path: '/home',
+      component: Home,
+      beforeEnter: (to, from, next) => {
+        if (isAuthenticated()) {
+          next();
+        } else {
+          next('/');
+        }
+      },
+      children: [
+        { path: 'input', component: Input },
+        { path: 'analyze', component: Analyze },
+        { path: '' },
+      ],
     },
-    children: [
-      { path: 'input', component: Input },
-      { path: 'analyze', component: Analyze },
-      { path: '', redirect: 'input' },
-    ],
-  },
-];
+  ];
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
 
-export default router;
+  export default router;
