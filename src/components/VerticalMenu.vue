132b3c4455245e8c3bdf29fb9268e348cd8d7c6a
ithiez <vovanthien1002@gmail.com>
2025-02-10 11:19:37 +0700

    menu

--- src/components/VerticalMenu.vue
+++ src/components/VerticalMenu.vue
@@ -1,20 +1,30 @@
 <template>
   <el-menu class="el-menu-vertical-demo" :collapse="isCollapse" :default-active="activeIndex">
     <!-- Input 頁面 -->
-    <el-menu-item index="1" @click="navigateTo('/mainPage/input')">
-      <el-icon><Edit /></el-icon>
-      <template #title>Input</template>
-    </el-menu-item>
-
+    <el-sub-menu index="1" @click="navigateTo('/mainPage/input')">
+      <template #title>
+        <el-icon>
+          <Edit />
+        </el-icon>
+        <span>Input</span>
+      </template>
+      <el-menu-item index="1-1">Water</el-menu-item>
+      <el-menu-item index="1-2">Energy</el-menu-item>
+    </el-sub-menu>
+    
     <!-- Analyze 頁面 -->
     <el-menu-item index="2" @click="navigateTo('/mainPage/analyze')">
-      <el-icon><DataAnalysis /></el-icon>
+      <el-icon>
+        <DataAnalysis />
+      </el-icon>
       <template #title>Analyze</template>
     </el-menu-item>
 
     <!-- 設定 -->
     <el-menu-item index="3" @click="navigateTo('/mainPage/settings')">
-      <el-icon><Setting /></el-icon>
+      <el-icon>
+        <Setting />
+      </el-icon>
       <template #title>Settings</template>
     </el-menu-item>
   </el-menu>
@@ -35,7 +45,7 @@ const activeIndex = computed(() => {
   if (route.path.includes("/mainPage/input")) return "1";
   if (route.path.includes("/mainPage/analyze")) return "2";
   if (route.path.includes("/mainPage/settings")) return "3";
-  return "1"; 
+  return "1";
 });
 
 const navigateTo = (path: string) => {
