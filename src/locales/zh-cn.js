bf1c9a48e355c9d6d65b3687852017339b55eaab
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2025-02-20 08:00:06 +0700

    fixed name factory

--- src/locales/zh-cn.js
+++ src/locales/zh-cn.js
@@ -2,9 +2,9 @@
 export default {
   login: {
     selectFactory: "选择工厂",
-    factoryTyXuan: "泰轩",
-    factoryTyBach: "泰白",
-    factoryTyThac: "泰塔克",
+    factoryTyXuan: "億春",
+    factoryTyBach: "億柏",
+    factoryTyThac: "億碩",
     userId: "账号",
     password: "密码",
     loginButton: "登录",
--- src/locales/zh-tw.js
+++ src/locales/zh-tw.js
@@ -2,9 +2,9 @@
 export default {
   login: {
     selectFactory: "選擇工廠",
-    factoryTyXuan: "泰軒",
-    factoryTyBach: "泰白",
-    factoryTyThac: "泰塔克",
+    factoryTyXuan: "億春",
+    factoryTyBach: "億柏",
+    factoryTyThac: "億碩",
     userId: "帳號",
     password: "密碼",
     loginButton: "登入",
